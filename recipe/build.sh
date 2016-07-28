609bb79e0b966b6a76ec10b9e799a28b7da0cb78
ocefpaf <ocefpaf@gmail.com>
2016-07-27 23:10:48 -0300

    Update to 1.7.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
+OPTS=""
+if [[ $(uname) == 'Darwin' ]]; then
   export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == Linux ]]; then
+  export OPTS="--disable-openmp"
+elif [[ $(uname) == 'Linux' ]]; then
   export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+  export CFLAGS="-fPIC -fopenmp $CFLAGS"
 fi
 
-export CPPFLAGS=-I$PREFIX/include
-export LDFLAGS=-L$PREFIX/lib
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+
 ./configure --prefix=$PREFIX \
             --disable-debug \
             --disable-dependency-tracking \
+            --with-fftw3 \
             --with-jasper=$PREFIX \
+            --with-grib_api=$PREFIX \
             --with-hdf5=$PREFIX \
             --with-netcdf=$PREFIX \
-            --with-proj=$PREFIX
+            --with-proj=$PREFIX \
+            --with-udunits2=$PREFIX \
+            --with-curl=$PREFIX \
+            --with-libxml2=$PREFIX \
+            $OPTS
 
 make
 eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
