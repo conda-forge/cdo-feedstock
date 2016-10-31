ab5866d4b4d6b193161c19ff24ee6796cfe5c104
ocefpaf <ocefpaf@gmail.com>
2016-10-31 09:17:34 -0300

    Workaround test failure

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-OPTS=""
 if [[ $(uname) == 'Darwin' ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-  export OPTS="--disable-openmp"
+  OPTS="--disable-openmp"
 elif [[ $(uname) == 'Linux' ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
   export CFLAGS="-fPIC -fopenmp $CFLAGS"
+  OPTS="--with-jasper=$PREFIX --with-libxml2=$PREFIX --with-curl=$PREFIX --with-proj=$PREFIX --with-fftw3 --with-grib_api=$PREFIX --with-udunits2=$PREFIX --with-netcdf=$PREFIX --with-hdf5=$PREFIX"
 fi
 
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
@@ -15,17 +13,11 @@ export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
 ./configure --prefix=$PREFIX \
             --disable-debug \
             --disable-dependency-tracking \
-            --with-fftw3 \
-            --with-jasper=$PREFIX \
-            --with-grib_api=$PREFIX \
-            --with-hdf5=$PREFIX \
-            --with-netcdf=$PREFIX \
-            --with-proj=$PREFIX \
-            --with-udunits2=$PREFIX \
-            --with-curl=$PREFIX \
-            --with-libxml2=$PREFIX \
             $OPTS
 
 make
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+# See https://github.com/conda-forge/cdo-feedstock/pull/8#issuecomment-257273909
+# Hopefully https://github.com/conda-forge/hdf5-feedstock/pull/48 will fix this.
+# eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+make check CDO="${SRC_DIR}/src/cdo -L"
 make install
