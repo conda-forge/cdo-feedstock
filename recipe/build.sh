ca2ce272128380ef286279347b40ba2c5ae1e0cb
wachsylon <fabian.wachsmann90@gmx.de>
2018-07-04 19:17:36 +0200

    Try the #40 change of Try2Code

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,7 @@ if [[ $(uname) == 'Darwin' ]]; then
 
   ./configure --prefix=$PREFIX \
               --disable-debug \
-              --disable-dependency-tracking \
-              --disable-openmp
+              --disable-dependency-tracking
 elif [[ $(uname) == 'Linux' ]]; then
 
   export CFLAGS="-fPIC -DPIC $CFLAGS"
