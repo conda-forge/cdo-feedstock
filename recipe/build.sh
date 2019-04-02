e23872e3ee4903f2fbbf731a79a2867c1a1bbbcb
Filipe Fernandes <ocefpaf@gmail.com>
2019-04-02 14:11:39 -0500

    fix #15 and no openmp on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
 if [[ $(uname) == 'Darwin' ]]; then
-
-  ./configure --prefix=$PREFIX \
-              --disable-debug \
-              --disable-dependency-tracking
-elif [[ $(uname) == 'Linux' ]]; then
-
-  export CFLAGS="-fPIC -DPIC $CFLAGS"
-  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp $CFLAGS"
-  export LDFLAGS="-L$PREFIX/lib -lhdf5 $LDFLAGS"
-  export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-  ./configure --prefix=$PREFIX \
-              --disable-debug \
-              --with-fftw3 \
-              --disable-dependency-tracking \
-              --with-libxml2=$PREFIX \
-              --with-curl=$PREFIX \
-              --with-proj=$PREFIX \
-              --with-eccodes=$PREFIX \
-              --with-udunits2=$PREFIX \
-              --with-netcdf=$PREFIX \
-              --with-hdf5=$PREFIX \
-              --with-ossp-uuid=$PREFIX 
+  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -stdlib=libc++ ${CFLAGS}"
+  ARGS="--disable-openmp"
+elif [[ $(uname) == Linux ]]; then
+  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp ${CFLAGS}"
+  export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
+  ARGS="--disable-dependency-tracking"
 fi
 
+./configure --prefix=${PREFIX} \
+            --host=${HOST} \
+            --build=${BUILD} \
+            --disable-debug \
+            --with-fftw3 \
+            --with-libxml2=${PREFIX} \
+            --with-curl=${PREFIX} \
+            --with-proj=${PREFIX} \
+            --with-eccodes=${PREFIX} \
+            --with-udunits2=${PREFIX} \
+            --with-netcdf=${PREFIX} \
+            --with-hdf5=${PREFIX} \
+            --with-ossp-uuid=${PREFIX} \
+            ${ARGS}
+
 make
-# See https://github.com/conda-forge/cdo-feedstock/pull/8#issuecomment-257273909
-# Hopefully https://github.com/conda-forge/hdf5-feedstock/pull/48 will fix this.
-# eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
-make check CDO="${SRC_DIR}/src/cdo -L"
+make check
 make install
