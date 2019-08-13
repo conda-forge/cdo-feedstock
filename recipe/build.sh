5e848961d8afbb11bfb50c0149ac3294c540e43c
Filipe Fernandes <ocefpaf@gmail.com>
2019-08-13 12:23:40 -0300

    try openmp on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,8 @@ export CFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1  ${CFLAGS}"
 
 if [[ $(uname) == 'Darwin' ]]; then
   export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -stdlib=libc++ ${CFLAGS}"
-  ARGS="--disable-openmp"
+  export LDFLAGS="${LDFLAGS} -fopenmp"
+  ARGS=""
 elif [[ $(uname) == Linux ]]; then
   export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp ${CFLAGS}"
   export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
