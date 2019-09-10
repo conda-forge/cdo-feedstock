88c2ef7f9e427aeb9240f2556c800ad4214c4aaa
wachsylon <fabian.wachsmann90@gmx.de>
2019-09-10 11:15:46 +0200

    Always need LIBS

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,10 @@ if [[ $(uname) == 'Darwin' ]]; then
   ARGS="--disable-openmp"
 elif [[ $(uname) == Linux ]]; then
   export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp ${CFLAGS}"
-  export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
-  export LIBS="-ljson-c"
   ARGS="--disable-dependency-tracking"
 fi
-
+  export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
+  export LIBS="-ljson-c"
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
             --build=${BUILD} \
