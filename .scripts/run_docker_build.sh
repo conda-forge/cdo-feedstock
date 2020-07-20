d8b6f28dd8b644225204657f3b73be91c9287284
wachsylon <fabian.wachsmann90@gmx.de>
2020-07-20 11:56:44 +0200

    MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.7.4, and conda-forge-pinning 2020.07.19.19.30.49

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -75,6 +75,8 @@ docker run ${DOCKER_RUN_ARGS} \
            -e FEEDSTOCK_NAME \
            -e CPU_COUNT \
            -e BINSTAR_TOKEN \
+           -e FEEDSTOCK_TOKEN \
+           -e STAGING_BINSTAR_TOKEN \
            $DOCKER_IMAGE \
            bash \
            /home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh
