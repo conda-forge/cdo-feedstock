2a24a32a6d42e0f02c9ec69d52f58d7a96e5bde1
wachsylon <fabian.wachsmann90@gmx.de>
2021-04-30 11:59:42 +0200

    Debugged build

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,11 +11,10 @@ elif [[ $(uname) == Linux ]]; then
   export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp ${CFLAGS}"
   export CFLAGS="${CFLAGS} -lm"
   export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
+  export LIBS="-ljson-c"
   ARGS="--disable-dependency-tracking"
 fi
 
-  export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
-  export LIBS="-ljson-c"
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
             --build=${BUILD} \
