6c599626e061cb0feafaff36c03717cf3a2d0989
wachsylon <fabian.wachsmann90@gmx.de>
2021-05-04 15:09:00 +0200

    Try c++14

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,11 @@
 export CFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1  ${CFLAGS}"
 
 if [[ $(uname) == 'Darwin' ]]; then
-  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -stdlib=libc++ ${CFLAGS}"
+  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++14 -stdlib=libc++ ${CFLAGS}"
   export LDFLAGS="${LDFLAGS} -fopenmp"
   ARGS=""
 elif [[ $(uname) == Linux ]]; then
-  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++11 -fopenmp ${CFLAGS}"
+  export CXXFLAGS="-fPIC -DPIC -g -O2 -std=c++14 -fopenmp ${CFLAGS}"
   export CFLAGS="${CFLAGS} -lm"
   export LDFLAGS="-L${PREFIX}/lib -lhdf5 ${LDFLAGS}"
   export LIBS="-ljson-c"
