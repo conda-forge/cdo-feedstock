69c83a02c0377bdce9bab50efb9b938a547527d6
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-12-13 15:22:16 +0100

    Cleanup build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 if [[ $(uname) == 'Darwin' ]]; then
-  # export CC=clang
-  # export CXX=clang++
-  # export CXXFLAGS="-fPIC -DPIC -g -O2 ${CFLAGS}"
   export CPP=clang-cpp
-  # export LDFLAGS="${LDFLAGS} -fopenmp"
   ARGS=""
 elif [[ $(uname) == Linux ]]; then
   export CXXFLAGS="-fPIC -DPIC -g -O2 -fopenmp ${CFLAGS}"
@@ -15,10 +11,6 @@ fi
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
-
-# if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 && $target_platform == "osx-arm64" ]]; then
-#     export ac_cv_search_H5Fopen=yes
-# fi
 
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
