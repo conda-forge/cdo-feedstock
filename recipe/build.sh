940e7462bb6119419700c520355f5b58eb66f8a7
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-12-13 12:42:23 +0100

    Add cached config key for configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,10 @@ fi
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 && $target_platform == "osx-arm64" ]]; then
+    export ac_cv_search_H5Fopen=yes
+fi
+
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
             --build=${BUILD} \
